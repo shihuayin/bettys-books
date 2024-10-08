32c51c4db82d87c58439cf59aae34521a40b6191
“Shihua <shihuaok@gmail.com>
2024-10-08 13:54:12 +0100

    lab 2 finish

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
-// Create a new router
+// Import required modules
 const express = require("express");
-const router = express.Router();
-
 const bcrypt = require("bcrypt");
 const mysql = require("mysql2");
+const router = express.Router();
 const saltRounds = 10;
 
+// Registration page
 router.get("/register", function (req, res, next) {
   res.render("register.ejs");
 });
@@ -15,10 +15,9 @@ router.post("/registered", function (req, res, next) {
 
   bcrypt.hash(plainPassword, saltRounds, function (err, hashedPassword) {
     if (err) {
-      return res.status(500).send("Error hashing password");
+      return res.status(500).send("Hashing failed");
     }
 
-    // Store hashed password in your database here
     const userData = {
       firstName: req.body.first,
       lastName: req.body.last,
@@ -39,41 +38,73 @@ router.post("/registered", function (req, res, next) {
 
     db.query(sql, values, function (err, result) {
       if (err) {
-        console.error("Error saving user data to the database:", err);
-        return res.status(500).send("Error saving user data");
+        console.error("Failed to save user data:", err);
+        return res.status(500).send("Failed to save user data");
       }
-      console.log("User data saved to the database:", result);
 
       let resultMessage =
         "Hello " +
         req.body.first +
         " " +
         req.body.last +
-        " you are now registered! We will send an email to you at " +
+        ", you have successfully registered! We will send an email to " +
         req.body.email;
-      resultMessage +=
-        " Your password is: " +
-        req.body.password +
-        " and your hashed password is: " +
-        hashedPassword;
       res.send(resultMessage);
     });
   });
 });
 
-// Get a list of all users (excluding passwords)
-router.get("/users/list", function (req, res, next) {
+// Login page
+router.get("/login", function (req, res, next) {
+  res.render("login.ejs");
+});
+
+router.post("/loggedin", function (req, res, next) {
+  const inputUsername = req.body.username;
+  const inputPassword = req.body.password;
+
+  const sql = "SELECT * FROM users WHERE username = ?";
+  db.query(sql, [inputUsername], function (err, results) {
+    if (err) {
+      console.error("Failed to retrieve user data from the database:", err);
+      return res.status(500).send("Failed to retrieve user data");
+    }
+
+    if (results.length === 0) {
+      return res
+        .status(401)
+        .send("Login failed: Incorrect username or password");
+    }
+
+    // Compare passwords
+    const hashedPassword = results[0].hashed_password;
+    bcrypt.compare(inputPassword, hashedPassword, function (err, result) {
+      if (err) {
+        console.error("Password comparison failed:", err);
+        return res.status(500).send("Password comparison failed");
+      }
+
+      if (result) {
+        res.send("Success, welcome back!");
+      } else {
+        res.status(401).send("Login failed: Incorrect username or password");
+      }
+    });
+  });
+});
+
+// Get user list
+router.get("/list", function (req, res, next) {
   const sql = "SELECT username, first_name, last_name, email FROM users";
 
   db.query(sql, function (err, results) {
     if (err) {
-      console.error("Error retrieving user data from the database:", err);
-      return res.status(500).send("Error retrieving user data");
+      console.error("Failed to retrieve user data:", err);
+      return res.status(500).send("Failed to retrieve user data");
     }
 
-    res.render("list.ejs", { users: results });
+    res.render("userlist.ejs", { users: results });
   });
 });
 
-// Export the router object so index.js can access it
 module.exports = router;
