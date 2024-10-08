22ef604975a44cc63d58e27f0e50b19c7533d004
“Shihua <shihuaok@gmail.com>
2024-10-08 12:57:31 +0100

    task 3 ok

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const bcrypt = require("bcrypt");
+const mysql = require("mysql2");
 const saltRounds = 10;
 
 router.get("/register", function (req, res, next) {
@@ -10,15 +11,68 @@ router.get("/register", function (req, res, next) {
 });
 
 router.post("/registered", function (req, res, next) {
-  // saving data in database
-  res.send(
-    " Hello " +
-      req.body.first +
-      " " +
-      req.body.last +
-      " you are now registered!  We will send an email to you at " +
-      req.body.email
-  );
+  const plainPassword = req.body.password;
+
+  bcrypt.hash(plainPassword, saltRounds, function (err, hashedPassword) {
+    if (err) {
+      return res.status(500).send("Error hashing password");
+    }
+
+    // Store hashed password in your database here
+    const userData = {
+      firstName: req.body.first,
+      lastName: req.body.last,
+      email: req.body.email,
+      username: req.body.username,
+      password: hashedPassword,
+    };
+
+    const sql =
+      "INSERT INTO users (username, first_name, last_name, email, hashed_password) VALUES (?, ?, ?, ?, ?)";
+    const values = [
+      userData.username,
+      userData.firstName,
+      userData.lastName,
+      userData.email,
+      userData.password,
+    ];
+
+    db.query(sql, values, function (err, result) {
+      if (err) {
+        console.error("Error saving user data to the database:", err);
+        return res.status(500).send("Error saving user data");
+      }
+      console.log("User data saved to the database:", result);
+
+      let resultMessage =
+        "Hello " +
+        req.body.first +
+        " " +
+        req.body.last +
+        " you are now registered! We will send an email to you at " +
+        req.body.email;
+      resultMessage +=
+        " Your password is: " +
+        req.body.password +
+        " and your hashed password is: " +
+        hashedPassword;
+      res.send(resultMessage);
+    });
+  });
+});
+
+// Get a list of all users (excluding passwords)
+router.get("/users/list", function (req, res, next) {
+  const sql = "SELECT username, first_name, last_name, email FROM users";
+
+  db.query(sql, function (err, results) {
+    if (err) {
+      console.error("Error retrieving user data from the database:", err);
+      return res.status(500).send("Error retrieving user data");
+    }
+
+    res.render("list.ejs", { users: results });
+  });
 });
 
 // Export the router object so index.js can access it
---
+++ user.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    hashed_password VARCHAR(255) NOT NULL
+);
