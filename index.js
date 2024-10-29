e7db3c1d5b08d193b67de11689401affbecf17c3
“Shihua <shihuaok@gmail.com>
2024-10-29 13:28:11 +0000

    add friend book list

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 var express = require("express");
 var ejs = require("ejs");
 
-//Import mysql module
+// Import mysql module
 var mysql = require("mysql2");
 
-//import session module
+// Import session module
 var session = require("express-session");
 
-//lab 4
+// Lab 4
 var validator = require("express-validator");
 
 const expressSanitizer = require("express-sanitizer");
@@ -25,7 +25,7 @@ app.set("view engine", "ejs");
 // Set up the body parser
 app.use(express.urlencoded({ extended: true }));
 
-// Set up public folder (for css and statis js)
+// Set up public folder (for CSS and static JS)
 app.use(express.static(__dirname + "/public"));
 
 // Set up session middleware
@@ -47,6 +47,7 @@ const db = mysql.createConnection({
   password: "qwertyuiop",
   database: "bettys_books",
 });
+
 // Connect to the database
 db.connect((err) => {
   if (err) {
@@ -71,13 +72,97 @@ app.use("/users", usersRoutes);
 const booksRoutes = require("./routes/books");
 app.use("/books", booksRoutes);
 
-// lab 5a Load the route handlers for /weather (weather route)
+// Load the route handlers for /weather (weather route)
 const weatherRoutes = require("./routes/weather");
 app.use("/", weatherRoutes);
 
-// lab 5b Load the route handlers for /api (API route for books)
+// Load the route handlers for /api (API route for books)
 const apiRoutes = require("./routes/api");
-app.use("/api", apiRoutes);
+app.use("/", apiRoutes); // 将 API 路由挂载在根路径上
 
 // Start the web app listening
 app.listen(port, () => console.log(`Node app listening on port ${port}!`));
+
+// // Import express and ejs
+// var express = require("express");
+// var ejs = require("ejs");
+
+// //Import mysql module
+// var mysql = require("mysql2");
+
+// //import session module
+// var session = require("express-session");
+
+// //lab 4
+// var validator = require("express-validator");
+
+// const expressSanitizer = require("express-sanitizer");
+
+// // Create the express application object
+// const app = express();
+// const port = 8000;
+
+// app.use(expressSanitizer());
+
+// // Tell Express that we want to use EJS as the templating engine
+// app.set("view engine", "ejs");
+
+// // Set up the body parser
+// app.use(express.urlencoded({ extended: true }));
+
+// // Set up public folder (for css and statis js)
+// app.use(express.static(__dirname + "/public"));
+
+// // Set up session middleware
+// app.use(
+//   session({
+//     secret: "somerandomstuff",
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: {
+//       expires: 600000, // Session expires in 600000 ms (10 minutes)
+//     },
+//   })
+// );
+
+// // Define the database connection
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "bettys_books_app",
+//   password: "qwertyuiop",
+//   database: "bettys_books",
+// });
+// // Connect to the database
+// db.connect((err) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log("Connected to database");
+// });
+// global.db = db;
+
+// // Define our application-specific data
+// app.locals.shopData = { shopName: "Bettys Books" };
+
+// // Load the route handlers
+// const mainRoutes = require("./routes/main");
+// app.use("/", mainRoutes);
+
+// // Load the route handlers for /users
+// const usersRoutes = require("./routes/users");
+// app.use("/users", usersRoutes);
+
+// // Load the route handlers for /books
+// const booksRoutes = require("./routes/books");
+// app.use("/books", booksRoutes);
+
+// // lab 5a Load the route handlers for /weather (weather route)
+// const weatherRoutes = require("./routes/weather");
+// app.use("/", weatherRoutes);
+
+// // lab 5b Load the route handlers for /api (API route for books)
+// const apiRoutes = require("./routes/api");
+// app.use("/api", apiRoutes);
+
+// // Start the web app listening
+// app.listen(port, () => console.log(`Node app listening on port ${port}!`));
--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const request = require("request");
 
-// all books
+// 本地书籍 API 路由
 router.get("/books", function (req, res, next) {
   let sqlquery = "SELECT * FROM books";
 
@@ -15,4 +16,37 @@ router.get("/books", function (req, res, next) {
   });
 });
 
+// 朋友的书籍列表 API 路由
+router.get("/friends-books", (req, res) => {
+  const url = "https://www.doc.gold.ac.uk/usr/108/api/books";
+
+  request(url, (err, response, body) => {
+    if (err) {
+      res.render("error", { message: "Unable to fetch friend's book list." });
+    } else {
+      const books = JSON.parse(body);
+      res.render("friends-books", { books });
+    }
+  });
+});
+
 module.exports = router;
+
+// const express = require("express");
+// const router = express.Router();
+
+// // all books
+// router.get("/books", function (req, res, next) {
+//   let sqlquery = "SELECT * FROM books";
+
+//   db.query(sqlquery, (err, result) => {
+//     if (err) {
+//       res.json(err);
+//       next(err);
+//     } else {
+//       res.json(result);
+//     }
+//   });
+// });
+
+// module.exports = router;
